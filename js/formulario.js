a29054873ac9cac2cc25be9af1122a88644aa96c
Nicolas Zapata <nicolaszapata@gmail.com>
2019-11-15 18:15:05 -0500

    Sitio funciona 0.9
    Textos funcionales pero sin afinar
    Form funcionando

---
+++ js/formulario.js
@@ -0,0 +1,42 @@
+
+
+/**
+ * Enviar formulario de contacto y mostrar respuesta
+ */
+function enviarForm()
+{
+
+   
+        var nombre = $("#nombre").val(),
+            email = $("#email").val(),
+            telefono = $("#telefono").val();
+           razon = $("#razonsocial").val();
+
+            if (!nombre) //la función se llama sola al cargar la página con campos vacios
+                return;
+        //Desactiva el boton para que no se envie el form dos veces
+        $("#botonEnviar").attr("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            url: 'https://h1iprt7feh.execute-api.us-east-1.amazonaws.com/Produccion',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'nombre': nombre,
+                'telefono': telefono,
+                'email': email,
+                'razon' : razon
+            }),
+            success: function(res)
+            {
+                alert("El formulario ha sido enviado exitosamente");
+            },
+            error: function(res)
+            {
+                alert("Lo lamentamos mucho. Ha ocurrido un error");
+                console.log(res);
+            }
+        });
+        return;
+
+}
